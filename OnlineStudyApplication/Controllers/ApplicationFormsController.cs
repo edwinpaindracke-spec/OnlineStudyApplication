a5bbff93fb538b588f7844b3267fd4325a09d60b
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2025-12-15 15:07:58 +0200

    adding a background colour and hiding the edit,delete and create from other users except the admin

--- OnlineStudyApplication/Controllers/ApplicationFormsController.cs
+++ OnlineStudyApplication/Controllers/ApplicationFormsController.cs
@@ -1,45 +1,87 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using OnlineStudyApplication.Data;
 using OnlineStudyApplication.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace OnlineStudyApplication.Controllers
 {
-    [Authorize] // must be logged in to apply
+    [Authorize]
     public class ApplicationFormsController : Controller
     {
         private readonly ApplicationDbContext _context;
-        private readonly UserManager<IdentityUser> _userManager;
 
-        public ApplicationFormsController(
-            ApplicationDbContext context,
-            UserManager<IdentityUser> userManager)
+        public ApplicationFormsController(ApplicationDbContext context)
         {
             _context = context;
-            _userManager = userManager;
         }
 
-        // =========================
-        // MY APPLICATIONS
-        // =========================
-        public async Task<IActionResult> MyApplications()
+        public class ApplicationsController : Controller
         {
-            var userId = _userManager.GetUserId(User);
+            private readonly ApplicationDbContext _context;
+            private readonly UserManager<IdentityUser> _userManager;
 
-            var applications = await _context.ApplicationForms
-                .Where(a => a.UserId == userId)
-                .ToListAsync();
+            public ApplicationsController(
+                ApplicationDbContext context,
+                UserManager<IdentityUser> userManager)
+            {
+                _context = context;
+                _userManager = userManager;
+            }
 
-            return View(applications);
+            public async Task<IActionResult> MyApplications()
+            {
+                var userId = _userManager.GetUserId(User);
+                var applications = _context.ApplicationForms
+                    .Where(a => a.UserId == userId)
+                    .ToList();
+
+                return View(applications);
+            }
         }
 
-        // =========================
-        // CREATE (APPLY)
-        // =========================
+        // GET: ApplicationForms
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ApplicationForms.ToListAsync());
+        }
 
-        // GET: ApplicationForms/Create?courseId=1
+        // GET: ApplicationForms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationForm = await _context.ApplicationForms
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (applicationForm == null)
+            {
+                return NotFound();
+            }
+
+            return View(applicationForm);
+        }
+
+        // GET: ApplicationForms/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ApplicationForms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // GET: ApplicationForms/Create
         public IActionResult Create(int courseId)
         {
             var application = new ApplicationForm
@@ -50,36 +92,94 @@ namespace OnlineStudyApplication.Controllers
             return View(application);
         }
 
-        // POST: ApplicationForms/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(ApplicationForm applicationForm)
+
+        // GET: ApplicationForms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
         {
-            if (!ModelState.IsValid)
+            if (id == null)
             {
-                return View(applicationForm);
+                return NotFound();
             }
 
-            applicationForm.UserId = _userManager.GetUserId(User);
-            applicationForm.Status = "Pending";
-
-            _context.ApplicationForms.Add(applicationForm);
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction(nameof(MyApplications));
+            var applicationForm = await _context.ApplicationForms.FindAsync(id);
+            if (applicationForm == null)
+            {
+                return NotFound();
+            }
+            return View(applicationForm);
         }
 
-        // =========================
-        // DETAILS (OPTIONAL)
-        // =========================
-        public async Task<IActionResult> Details(int? id)
+        // POST: ApplicationForms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,CourseId,FullName,Email,Education,Status")] ApplicationForm applicationForm)
         {
-            if (id == null) return NotFound();
+            if (id != applicationForm.Id)
+            {
+                return NotFound();
+            }
 
-            var applicationForm = await _context.ApplicationForms.FindAsync(id);
-            if (applicationForm == null) return NotFound();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(applicationForm);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApplicationFormExists(applicationForm.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(applicationForm);
+        }
+
+        // GET: ApplicationForms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var applicationForm = await _context.ApplicationForms
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (applicationForm == null)
+            {
+                return NotFound();
+            }
 
             return View(applicationForm);
+        }
+
+        // POST: ApplicationForms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var applicationForm = await _context.ApplicationForms.FindAsync(id);
+            if (applicationForm != null)
+            {
+                _context.ApplicationForms.Remove(applicationForm);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ApplicationFormExists(int id)
+        {
+            return _context.ApplicationForms.Any(e => e.Id == id);
         }
     }
 }
